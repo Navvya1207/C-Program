04236822a64b1bdfdb07f3c8f0505ba21ed2b387
Navvya Bhardwaj <navvya.bhardwaj12jul@gmail.com>
2025-08-14 11:44:17 +0530

    Add files via upload

---
+++ class 1 prg 2.c
@@ -0,0 +1,12 @@
+// WAP to take 2 numbera as input and print sum
+#include <stdio.h>
+int main(){
+	int num1,num2,sum;
+	printf("Enter num 1");
+	scanf("%d",&num1);
+	printf("Enter num 2");
+	scanf("%d", &num2);
+	sum=num1+num2;
+	printf("Sum= %d",sum);
+	return 0;
+}
